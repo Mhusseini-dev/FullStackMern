b37e2d142e8511252a2a16ba8bdfe01283106d5f
Mhusseini-dev <74101409+Mhusseini-dev@users.noreply.github.com>
2021-02-10 15:48:14 +0200

    Delete products-manager directory

--- products-manager/server/routes/person.routes.js
+++
@@ -1,6 +0,0 @@
-const PersonController = require('../controllers/person.controller');
-module.exports = function(app){
-    app.get('/api/people', PersonController.all);
-    app.post('/api/people', PersonController.createPerson);
-    app.get('/api/people/:id', PersonController.getPerson);
-}
